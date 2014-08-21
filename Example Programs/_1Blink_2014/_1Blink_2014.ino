f207424a8552dfb929730a0a80ff1bace6c423f2
HC Student <hcstudent@HC-Lab-5.local>
2014-08-21 13:25:27 -0400

    Added comments to multiple filles and edited and simplefied them

--- Example Programs/_1Blink_2014/_1Blink_2014.ino
+++ Example Programs/_1Blink_2014/_1Blink_2014.ino
@@ -1,12 +1,12 @@
-int led = 3;
+int led = 3;     //Declares Var led to pin 3
 void setup()
 {
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);    //Declares pin 3 to be an output   
 }
-for(x, x < 10, x++)
+for(x, x < 10, x++)   //repeats while x is < 10 x goes up by a constant 1
 {
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
+  digitalWrite(led, HIGH); //sets led on HIGH
+  delay(1000);    //waits 1 sec
+  digitalWrite(led, LOW);    //sets led to LOW
+  delay(1000);   //waits 1 sec
 }
--- Example Programs/_2FrontSensorTest_2014/_2FrontSensorTest_2014.ino
+++ Example Programs/_2FrontSensorTest_2014/_2FrontSensorTest_2014.ino
@@ -1,16 +1,13 @@
-/*
-
-  This example code is in the public domain.
- */
+//This example code is in the public domain.
  
- #include "DigiKeyboard.h" 
+#include "DigiKeyboard.h" 
  
 int front_sensor_val = 0; 
 
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the outputs.
+  // initialize the outputs or inputs.
   pinMode(3,OUTPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);  
@@ -19,16 +16,18 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
 
-  int front_sensor_val = analogRead(2);  
+  int front_sensor_val = analogRead(2);  //sets val of front sensor val to pin 2 values.
   
-  if (front_sensor_val > 500){
-    digitalWrite(3,HIGH);
-    delay(100);
-    digitalWrite(3,LOW);
-    delay(100);}
-  else{
-    digitalWrite(3,LOW);}
+  if (front_sensor_val > 500)  //while te val of the front sensor is greater then 500 do,
+  {    
+    digitalWrite(3,HIGH);   //sets pin 3 to HIGH
+    delay(100);  //waits .1sec
+    digitalWrite(3,LOW);  //sets pin to LOW
+    delay(100); //waits .1 sec
+  }
+  else{     //if it is < 500 do,
+    digitalWrite(3,LOW);}    //sets pin 3 to LOW
   
-  //DigiKeyboard.println(down_sensor_val);
-  //DigiKeyboard.println(front_sensor_val);
+  //DigiKeyboard.println(down_sensor_val);    //prints values from var down sensor
+  //DigiKeyboard.println(front_sensor_val);   //prints values from var front sensor
 }
